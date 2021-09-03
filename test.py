ed9a6541e8df2f817b1a9fa1e5b57aadee87c9bf
andresawa <75858683+andresawa@users.noreply.github.com>
2021-09-03 18:46:28 -0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,9 @@
+if (Fco2_ponto1 + Fco2_ponto2 == -5.944):
+    print("O calculo tá certo, você pode continuar com o proximo código")
+    teste = True
+elif (Fco2_ponto1 == -2.138):
+    print("Opa, algo deu errado com o ponto 2, recorde verificar as unidades e só usar 3 decimais")
+elif (Fco2_ponto2 == -3.806):
+    print("Opa, algo deu errado com o ponto 1, recorde verificar as unidades e só usar 3 decimais")
+else:
+    print("Opa, algo deu errado com os dois pontos, recorde verificar as unidades e só usar 3 decimais")
