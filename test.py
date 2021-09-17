7a0db35d2a04cd785123d4970223ec8ce4ba8b74
andresawa <75858683+andresawa@users.noreply.github.com>
2021-09-17 11:28:13 -0300

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ if (Fco2_ponto1 + Fco2_ponto2 == -5.944):
     teste = True
 elif (Fco2_ponto1 == -2.138):
     print("Opa, algo deu errado com o ponto 2, recorde verificar as unidades e só usar 3 decimais")
-elif (Fco2_ponto2 == -3.806):
+elif (Fco2_ponto2 == -3.821):
     print("Opa, algo deu errado com o ponto 1, recorde verificar as unidades e só usar 3 decimais")
 else:
     print("Opa, algo deu errado com os dois pontos, recorde verificar as unidades e só usar 3 decimais")
